32664b179cbba04897d89b305da88a41e6c366e2
anne <anne.gemma.lee@gmail.com>
2024-05-23 11:25:50 +0200

    add lab12

---
+++ ranges.py
@@ -0,0 +1,15 @@
+for num in range(4):
+  print(num)
+
+for numero in range(2, 10, 3):
+    print(numero)
+
+
+users = {
+    'Juan': 23,
+    'Marta': 16,
+    'Pedro': 34,
+    'Laura': 41
+}
+for key, value in users.items():
+  print(key, 'is', value, 'years old')
---
+++ test.py
@@ -0,0 +1,35 @@
+# def count_by(x, n):
+#     ans = []
+#     for num in range(x, x*n + 1, x):
+#         ans.append(num)
+#     print(ans)        
+        
+# count_by(1, 10)
+# count_by(2, 5)
+
+# ========================================
+  
+# using a loop to add up a numbers of a list
+# ans = 0
+# list = [1, 3, 5, 6, 2]
+
+# for number in list:
+#   ans += number
+  
+# print(ans)
+
+# ========================================
+
+#calcular el volumen de cilindro
+
+import math
+
+def cilindroVolumen (r, h):
+  return r * r * math.pi * h
+
+print(cilindroVolumen(2, 4))
+
+
+cilindroVolumen = lambda r, h: r * r * math.pi * h
+print(cilindroVolumen(2, 4))
+
