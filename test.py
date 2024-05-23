f1871fc1d1a490b6247bd772ad439adf5cd5acd5
anne <anne.gemma.lee@gmail.com>
2024-05-23 14:13:03 +0200

    add lab13

--- test.py
+++ test.py
@@ -2,34 +2,59 @@
 #     ans = []
 #     for num in range(x, x*n + 1, x):
 #         ans.append(num)
-#     print(ans)        
-        
+#     print(ans)
+
 # count_by(1, 10)
 # count_by(2, 5)
 
 # ========================================
-  
+
 # using a loop to add up a numbers of a list
 # ans = 0
 # list = [1, 3, 5, 6, 2]
 
 # for number in list:
 #   ans += number
-  
+
 # print(ans)
 
 # ========================================
 
-#calcular el volumen de cilindro
+# calcular el volumen de cilindro
 
-import math
+# import math
 
-def cilindroVolumen (r, h):
-  return r * r * math.pi * h
+# def cilindroVolumen (r, h):
+#   return r * r * math.pi * h
 
-print(cilindroVolumen(2, 4))
+# print(cilindroVolumen(2, 4))
 
 
-cilindroVolumen = lambda r, h: r * r * math.pi * h
-print(cilindroVolumen(2, 4))
+# cilindroVolumen = lambda r, h: r * r * math.pi * h
+# print(cilindroVolumen(2, 4))
+
+# ========================================
+
+
+# -----map-----
+numeros = (1, 2, 3, 4, 5)
+
+def cubico(number):
+    return number**3
+
+result = map(cubico, numeros)
+print(list(result))
+
+# resultLambda = map(lambda cubico, numeros: )
+
+# -----filter-----
+numeros = [1, 2, 3, 4, 5]
+result = filter(lambda x: x > 5, numeros)
+
+# -----r-educe-----
+from functools import reduce
+
+numeros = [1, 2, 3, 4, 5]
+result = reduce(lambda x, y: x + y, numeros)
+print(result)
 
